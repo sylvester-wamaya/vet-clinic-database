29474268dc1c0b2baff46a31963a8d8763f20ea9
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-19 03:01:39 +0300

    Add querries on aggregate querries.

--- queries.sql
+++ queries.sql
@@ -30,3 +30,12 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO SP1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(*) AS num_escaped FROM animals WHERE escape_attempts > 0 GROUP BY neutered;
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
