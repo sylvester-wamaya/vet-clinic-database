fff10f44bce17c4d8415f9e96100fdd792606375
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-20 22:22:06 +0300

    Add data in the specializations table

--- data.sql
+++ data.sql
@@ -65,3 +65,6 @@ INSERT INTO vets (name, age, date_of_graduation)
         ('Maisy Smith', 26, '2019-01-17'),
         ('Stephanie Mendez', 64, '1981-05-04'),
         ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (species_id, vet_id)
+    VALUES(1, 1), (1, 3), (2, 3), (2, 4);
