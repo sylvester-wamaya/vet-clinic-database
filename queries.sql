d153d64fc472314a85448303a1f28fbfcbe0d32e
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-21 02:52:12 +0300

    Add querries

--- queries.sql
+++ queries.sql
@@ -49,3 +49,51 @@ SELECT O.full_name, COUNT(A.name) FROM animals A
 JOIN owners O  ON A.owner_id  = O.id
 GROUP BY O.full_name ORDER BY COUNT(A.name)
 DESC LIMIT 1;
+
+SELECT A.name  FROM animals A
+JOIN visits V ON A.id = V.animals_id 
+JOIN vets T ON V.vets_id = T.id
+WHERE T.name LIKE 'William Tatcher'
+ORDER BY V.date_of_visit ASC LIMIT 1;
+
+SELECT COUNT(A.name)  FROM animals A
+JOIN visits V ON A.id = V.animals_id 
+JOIN vets T ON V.vets_id = T.id
+WHERE T.name LIKE 'Stephanie Mendez';
+
+SELECT V.name AS vet_name, S.name AS species_name FROM species S
+FULL JOIN specializations Z ON S.id = Z.species_id 
+FULL JOIN vets V ON Z.vet_id = V.id;
+
+SELECT A.name FROM animals A
+JOIN visits V ON A.id = V.animals_id 
+JOIN vets T ON V.vets_id = T.id
+WHERE T.name LIKE 'Stephanie Mendez'
+AND V.date_of_visit BETWEEN '2020-04-01' AND '2020-08-01';
+
+SELECT A.name, COUNT(V.date_of_visit) FROM animals A
+JOIN visits V ON A.id = V.animals_id GROUP BY A.name
+ORDER BY COUNT(V.date_of_visit) DESC LIMIT 1;
+
+SELECT A.name FROM animals A
+JOIN visits V ON A.id = V.animals_id JOIN vets T ON
+V.vets_id = T.id WHERE T.name LIKE 'Maisy Smith'
+ORDER BY V.date_of_visit ASC LIMIT 1;
+
+SELECT A.name AS animal_name, A.date_of_birth, A.escape_attempts,
+A.neutered, A.weight_kg, T.name AS vet_name, T.age AS vet_age,
+T.date_of_graduation AS vets_garduation_date, V.date_of_visit 
+FROM animals A JOIN visits V ON A.id = V.animals_id JOIN vets T ON
+V.vets_id = T.id ORDER BY V.date_of_visit DESC LIMIT 1;
+
+SELECT T.name, COUNT(V.date_of_visit) FROM visits V
+FULL JOIN vets T ON V.vets_id = T.id FULL JOIN specializations S ON
+T.id = S.vet_id WHERE S.species_id IS NULL
+ GROUP BY T.name;
+
+ SELECT S.name FROM visits V
+JOIN vets T ON V.vets_id = T.id
+JOIN animals A ON A.id = V.animals_id
+JOIN species S ON A.species_id = S.id
+WHERE T.name LIKE 'Maisy Smith' GROUP BY S.name
+ORDER BY COUNT(S.name) DESC LIMIT 1;
